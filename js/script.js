04aa29fc2d1fdc067197dd9ef7f3868d95c6e20b
Delson Júnior <delson.junior@mobi2buy.com>
2021-03-05 07:57:04 -0300

    change project

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ import initAnimacaoScroll from './modules/scroll-animacao.js'
 import initScrollSuave from './modules/scroll-suave.js'
 import initAccordion from './modules/accordion.js'
 import menuTab from './modules/menutab.js'
+import initModal from './modules/modal.js'
+import initTootip from './modules/tootip.js'
 
 initAnimacaoScroll();
 initScrollSuave();
 initAccordion();
 menuTab();
+initModal();
+initTootip();
